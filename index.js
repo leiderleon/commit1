5520c35ba4f5b599260fb96e318c948bdda1a2eb
leider <leiderleon2006@gmail.com>
2024-11-08 15:11:39 -0500

    commit2

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const express = require("express");
 const app = express();
 const path = require("path")
+const colors = require("colors");
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "iniciosesion.html"));
+// Configuracion del motor de plantillas
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+
+
+// Rutas
+
+app.get("/iniciosesion", (req, res) => {
+    res.render('iniciosesion')
+    console.log("Entrando a inicio de sesion".green);
 });
 
 app.get("/welcome", (req, res) => {
-    res.sendFile(path.join(__dirname, "welcome.html"));
+    res.render('welcome')
+    console.log("Entrando a bienvenida".yellow);
 });
 
 app.get("/info-user", (req, res) => {
-    res.sendFile(path.join(__dirname, "info-user.html"));
+    res.render('info-user')
+    console.log("Entrando a info-user".red);
 });
+
+// Ejecucion server
 
 app.listen(3000, () => {
     console.log("Servidor iniciado en el puerto 3000");
